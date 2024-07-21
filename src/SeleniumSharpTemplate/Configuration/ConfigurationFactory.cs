ddff21f52446c1d55f8f524824124853232e9573
marzooq23 <mohamedmarzooq23@gmail.com>
2024-07-21 11:16:09 +0530

    adding cross browser test fixture

--- src/SeleniumSharpTemplate/Configuration/ConfigurationFactory.cs
+++ src/SeleniumSharpTemplate/Configuration/ConfigurationFactory.cs
@@ -2,7 +2,7 @@
 {
     public static class ConfigurationFactory
     {
-        public static T? Create<T>(string jsonFilePath) => 
+        public static T? Create<T>(string jsonFilePath) =>
             JsonSerializer.Deserialize<T>(File.ReadAllText(jsonFilePath));
     }
 }
