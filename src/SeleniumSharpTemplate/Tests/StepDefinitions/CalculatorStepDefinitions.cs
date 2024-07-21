4700c9c61356484d686f1bda0c26336f0c44a6d3
marzooq23 <mohamedmarzooq23@gmail.com>
2024-07-21 15:59:08 +0530

    clean up

--- src/SeleniumSharpTemplate/Tests/StepDefinitions/CalculatorStepDefinitions.cs
+++ src/SeleniumSharpTemplate/Tests/StepDefinitions/CalculatorStepDefinitions.cs
@@ -1,13 +1,13 @@
 ﻿namespace SeleniumSharpTemplate.Tests.StepDefinitions
 {
     [Binding]
-    public sealed class CalculatorStepDefinitions(Config.Config config, GoogleHomePage googleHomePage)
+    public sealed class CalculatorStepDefinitions(GoogleHomePage googleHomePage)
     {
         [Given("I launch google calculator")]
         public void GivenILaunchGoogleCalculator()
         {
             const string searchText = "calculator";
-            googleHomePage.GoToUrl(config.UrlGoogle!);
+            googleHomePage.GoToGoogle();
             googleHomePage.Search(searchText);
             googleHomePage.GetPageTitle.Should().StartWith(searchText);
         }
--- src/SeleniumSharpTemplate/Utilities/Configuration/ConfigurationFactory.cs
+++ src/SeleniumSharpTemplate/Utilities/Configuration/ConfigurationFactory.cs
@@ -2,9 +2,6 @@
 {
     public static class ConfigurationFactory
     {
-        public static T? Create<T>(string jsonFilePath) =>
-            JsonSerializer.Deserialize<T>(File.ReadAllText(jsonFilePath));
-
         public static T? GetBinding<T>(string jsonFileName, string section) =>
             new ConfigurationBuilder()
             .AddJsonFile(jsonFileName)
